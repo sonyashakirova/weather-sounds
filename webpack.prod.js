e91df5c8a74114ebf11a30efc0815637346c2e4b
Sonya Shakirova <smshakirova@mail.ru>
2023-06-03 18:56:00 +0300

    build: add optimizations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,29 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 const { merge } =  require("webpack-merge")
 const commonConfig = require("./webpack.common")
 
 module.exports = merge(commonConfig, {
-  mode: "production"
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // оптимизирует css
+      new CssMinimizerPlugin(),
+      // оптимизирует js
+      new TerserPlugin()
+    ],
+    // выносит node_modules в отдельный файл
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          chunks: "all",
+          name: "vendor",
+          test: /node_modules/
+        }
+      }
+    }
+  }
 })
