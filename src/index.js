031adaea229ab90539fae4b0708bbf3d28b614c4
Sonya Shakirova <smshakirova@mail.ru>
2023-06-04 19:27:00 +0300

    feat: render list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import { soundObjectList } from "./data"
 import "./index.scss"
+
+const soundList = document.getElementById("list")
+
+soundObjectList.forEach((soundParams) => {
+  const listItem = document.createElement("li")
+  listItem.textContent = soundParams.code
+  soundList.appendChild(listItem)
+})
